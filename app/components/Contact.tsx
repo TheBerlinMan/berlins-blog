9fe9e0acf6a00630c422d0cbffa18bd86b14c97d
Berlin <tommyonik@gmail.com>
2025-01-20 16:30:48 -0500

    add tooltip to icons

--- app/components/Contact.tsx
+++ app/components/Contact.tsx
@@ -1,15 +1,47 @@
-import React from 'react'
+import React from "react";
+import { Github, Instagram, Mail } from "lucide-react";
+import {
+  Tooltip,
+  TooltipTrigger,
+  TooltipContent,
+  TooltipProvider,
+} from "@/app/components/tooltip";
+
+interface IconTooltipProps {
+  icon: React.ReactNode;
+  content: string;
+}
+
+const IconTooltip = ({ icon, content }: IconTooltipProps) => {
+  return (
+    <TooltipProvider>
+      <Tooltip>
+        <TooltipTrigger>{icon}</TooltipTrigger>
+        <TooltipContent>
+          <p>{content}</p>
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
+  );
+};
 
 const Contact = () => {
   return (
-    <div className='flex flex-col'>
-        <p>Email : tommyonik at gmail dot com</p>
-        <a href="https://github.com/TheBerlinMan">Github : @TheBerlinMan </a>
-        <a href="https://www.instagram.com/im.tnma/">Instagram : @im.tnma</a>
-        {/* <a href="https://x.com/TheBerlinMan">Twitter / X : @TheBerlinMan</a>
-        <a href="https://www.linkedin.com/in/thomas-onik/">LinkedIn : @Thomas Onik </a> */}
+    <div className="flex flex-col gap-2">
+      <div className="flex gap-3">
+        <IconTooltip icon={<Github />} content="GitHub" />
+        <a href="https://github.com/TheBerlinMan">@TheBerlinMan </a>
+      </div>
+      <div className="flex gap-3">
+        <IconTooltip icon={<Instagram />} content="Instagram" />
+        <a href="https://www.instagram.com/im.tnma/">@im.tnma</a>
+      </div>
+      <div className="flex gap-3">
+        <IconTooltip icon={<Mail />} content="Email" />
+        <p>tommyonik at gmail dot com</p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
